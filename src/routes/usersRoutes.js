47d3e3779d7efbceba11dc6037c408a3937893fe
maria parma <mariaparma.tds1.senai@gmail.com>
2025-02-25 09:32:52 -0300

    feat: update const in  routes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const usersController = require("../controllers/usersController");
+const usersController = require("../controllers/usersControllers");
 
 router.get("/", usersController.getAllUsers);
 router.post("/", usersController.addUser);
