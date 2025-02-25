6bd8f409dd12774081993b22d008286ff10afbc5
maria parma <mariaparma.tds1.senai@gmail.com>
2025-02-25 09:25:15 -0300

    feat create users

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const usersRoutes = require("./src/routes/usersRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use ("/api", usersRoutes);
+
+app.get("/", (req, res) => {
+    res.send("EUUUU AMOOOO DEMAISSSS BACKEND <3");
+});
+
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
